6076302bdb0ca7808999017cd4b2fd360b463bcd
Ars <ars9000@gmail.com>
2022-07-12 15:24:59 +0300

    finish 2 Home Work agen)

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,10 +1,10 @@
 ﻿
 // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 /*
-int medium(int nam)
+int medium(int num)
 {
-    if (nam > 999 || nam < 100) return -1;
-    return nam / 10 % 10;
+    if (num > 999 || num < 100) return -1;
+    return num / 10 % 10;
 }
 Console.Write("Введите 3х значное число: ");
 int UserNum = Convert.ToInt32(Console.ReadLine());
@@ -15,19 +15,20 @@ else Console.Write("Средняя цифра: " + medium(UserNum));
 //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 int ThirNum(int num)
 {
-    if (nam > 999 || nam < 100) return -1;
-    return nam % 10;
+    if (num < 999 ) return -1;
+    while (num>999) num = num /10;
+    return num % 10;
 }
-Console.Write("Введите 3х значное число: ");
+Console.Write("Свас число с меня его 3я цифра: ");
 int UserNum = Convert.ToInt32(Console.ReadLine());
-if (ThirNum(UserNum) == -1) Console.Write("Ошибка, вы ввели не 3х значное число");
-else Console.Write("Средняя цифра: " + ThirNum(UserNum));
+if (ThirNum(UserNum) == -1) Console.Write("Ошибка, вы ввели число в котором нет 3ей цифры. ");
+else Console.Write("Третья цифра: " + ThirNum(UserNum));
 */
 
 //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 string weekdey(int num)
 {
-    if (num < 0 || num > 7) return "Ошибка, вы ввели число вне диапозона!";
+    if (num < 1 || num > 7) return "Ошибка, вы ввели число вне диапозона!";
     if (num > 0 && num < 6) return "Будний день,идем работать.";
     if (num == 6) return "Шабат шалом!;)";
     return "Ура ! Выходной!";
